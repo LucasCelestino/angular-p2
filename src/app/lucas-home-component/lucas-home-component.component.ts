2fa861d3179b8e41fa9e6484e88f1e681ac8c82a
LucasCelestino <lucascap899@gmail.com>
2022-06-17 08:14:14 -0300

    5 files added

---
+++ src/app/lucas-home-component/lucas-home-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lucas-home-component',
+  templateUrl: './lucas-home-component.component.html',
+  styleUrls: ['./lucas-home-component.component.css']
+})
+export class LucasHomeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/lucas-tasks-component/lucas-tasks-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lucas-tasks-component',
+  templateUrl: './lucas-tasks-component.component.html',
+  styleUrls: ['./lucas-tasks-component.component.css']
+})
+export class LucasTasksComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
