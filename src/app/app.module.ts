d14a1e22972230bdc62142e2253158e463f4af38
LucasCelestino <lucascap899@gmail.com>
2022-06-17 08:19:35 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { LucasHomeComponentComponent } from './lucas-home-component/lucas-home-component.component';
+import { LucasTasksComponentComponent } from './lucas-tasks-component/lucas-tasks-component.component';
+import { LucasTasksServiceService } from './lucas-tasks-service.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: LucasHomeComponentComponent },
+      { path: 'tarefas', component: LucasTasksComponentComponent },
+    ]),
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [LucasTasksServiceService],
 })
-export class AppModule { }
+export class AppModule {}
