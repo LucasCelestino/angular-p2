0168cae1abba634b93127c1308914a6f499c0421
LucasCelestino <lucascap899@gmail.com>
2022-06-17 08:13:31 -0300

    1 file modified, 1 added

---
+++ src/app/lucas-tasks-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LucasTasksServiceService {
+  myTaks = localStorage;
+
+  constructor() {}
+
+  getAllTasks() {
+    var values = [],
+      keys = Object.keys(this.myTaks),
+      i = keys.length;
+
+    while (i--) {
+      values.push(this.myTaks.getItem(keys[i]));
+    }
+
+    return values;
+  }
+
+  createTask(task: string) {
+    this.myTaks.setItem(this.myTaks.length.toString(), task);
+  }
+
+  deleteTask(key: number) {
+    this.myTaks.removeItem(key.toString());
+  }
+}
